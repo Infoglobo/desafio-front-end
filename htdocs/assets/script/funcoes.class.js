32ff127e17743ca8d4c33b8ab6ce29840d597179
rafaelc85 <rafaelc85@gmail.com>
2017-05-14 14:24:16 -0300

    Simplificação do código
    melhoras no arquivo principal do css
    simplificação do código javascript

--- htdocs/assets/script/funcoes.class.js
+++ htdocs/assets/script/funcoes.class.js
@@ -1,35 +1,36 @@
 // classe para carregar dados do arquivo json e formata-los apropriadamente
 function json(){
-	var qtdSection;
-	var retorno;
-	var artigos;
-	var classeExtra; 
+	var qtdSection, sectionName, retorno, artigos, breakSize=520;
+	var classeExtra, image, title, label, url, description;
 	
 	// Resgatar valores
 	json.prototype.resgatarValores = function(){
-		$('#resultado').html('Carregando dados...');
+		$('#main').html('Carregando dados...');
 
 		// Estrutura de resultado.
 		$.getJSON('../../data.json', function(dados){
 			this.qtdSection = dados.section.length;
 			
-			//varrendo as informacoes para fazer o tratamento
+			//varrendo as informacoes do arquivo json para fazer o tratamento
 			for (i = 0; i < this.qtdSection; i++){
 				this.retorno = '';
-				classeExtra = '';
-				artigos = dados.section[i].data;							
+				artigos = dados.section[i].data;
+				sectionName = dados.section[i].name;							
+				
 				for (j = 0; j < artigos.length; j++){
-					//personalizando a session destaque
-					if(dados.section[i].name == "main"){
+					//separando dados em variaveis para simplificar codigo
+					image = dados.section[i].data[j].image;
+					title = dados.section[i].data[j].title;
+					label = dados.section[i].data[j].label;
+					url = dados.section[i].data[j].url;
+					description = dados.section[i].data[j].description;
+
+					//personalizando a secao destaque
+					if(sectionName == "main"){
 						if(j<2) {
-							if ($(window).width()>520){ 
-								//codigo a parte para os dois primeiros artigos da secao destaque, em caso de usuario desktop
-								this.retorno += '<div class="coluna top" style="background: url(assets/media/' + dados.section[i].data[j].image + '">';
-								this.retorno += '<div id="caixaArtigo">'; 
-								this.retorno += '<p class="label">' + dados.section[i].data[j].label + '</p>';
-								this.retorno += '<h2><a href=" ' + dados.section[i].data[j].url + '" title="' + dados.section[i].data[j].title + '">' + dados.section[i].data[j].title + '</a></h2>';
-								this.retorno += '<p class="description">' + dados.section[i].data[j].description + '</p>';
-								this.retorno += '</div></div>';
+							if ($(window).width()>breakSize){ 
+								//monta html da chamada para os dois primeiros artigos da secao destaque, em caso de usuario desktop
+								this.retorno = montaColunaDestaqueTop(this.retorno, image, title, label, url, description);								
 								continue;
 							}else classeExtra = "top";							
 						} 
@@ -37,48 +38,52 @@ function json(){
 						else classeExtra = "bot";
 					}
 					//criando div para paginacao de 4 itens
-					if(j==4){
-						if(dados.section[i].name == "Brasil") this.retorno += '<div id="brasilHidden">';
-						if(dados.section[i].name == "Mundo") this.retorno += '<div id="mundoHidden">';
-					}				
-	  				this.retorno += '<div class="coluna ' + classeExtra + '">';
-					this.retorno += '<figure class="foto"><img src="assets/media/' + dados.section[i].data[j].image + '" alt="' + dados.section[i].data[j].title + '" title="' + dados.section[i].data[j].title + '"><div class="imgbox"></div></figure>';
-					this.retorno += '<p class="label">' + dados.section[i].data[j].label + '</p>';
-					this.retorno += '<h2><a href=" ' + dados.section[i].data[j].url + '" title="' + dados.section[i].data[j].title + '">' + dados.section[i].data[j].title + '</a></h2>';
-					this.retorno += '<p class="description">' + dados.section[i].data[j].description + '</p>';
-					this.retorno += '</div>';	
+					if(j==4 && sectionName != "main") this.retorno += '<div id="'+ sectionName +'Hidden" class="hidden">';					
+					
+					//montando as chamadas dos demais artigos em html
+					this.retorno = montaColuna(this.retorno, classeExtra, image, title, label, url, description);	
 				}
+				
 				//finalizando o codigo html e inserindo nas divs da view
-				switch (dados.section[i].name) {
-					case "main":
-						$('#main').html(this.retorno);
-					break;
-					case "Brasil":
-						this.retorno += '</div>'; //fechando div brasilHidden
-						this.retorno = '<h1>Brasil</h1>' + this.retorno;
-						this.retorno += '<div class="sessionBot" onclick="toggle_visibility(\'brasilHidden\');"><a href="javascript:">Brasil +</a></div>';
-						$('#brasil').html(this.retorno);
-					break;
-					case "Mundo":
-						this.retorno += '</div>'; //fechando div mundoHidden
-						this.retorno = '<h1>Mundo</h1>' + this.retorno;
-						this.retorno += '<div class="sessionBot" onclick="toggle_visibility(\'mundoHidden\');"><a href="javascript:">Mundo +</a></div>';
-						$('#mundo').html(this.retorno);
-					break;
-				}
+				if(dados.section[i].name != "main") {
+					this.retorno = '<h1>'+ sectionName +'</h1>' + this.retorno;
+					this.retorno += '</div>'; //fechando div de paginacao
+					this.retorno += '<div class="sessionBot" onclick="toggle_visibility(\''+ sectionName +'Hidden\');"><a href="javascript:">' + sectionName +' +</a></div>';
+					$('#'+ sectionName.toLowerCase() +'').html(this.retorno);
+				} else $('#main').html(this.retorno);
 				
 			}
 			//escrevendo informacoes de anuncio na view
-			setAdvertisingInformation();					
+			setAdvertisingInformation(breakSize);					
 		});
-
 	}
-
 }
 
 // inicializando Objeto.
 var obj = new json();
 obj.resgatarValores();
+
+//funcao para montar uma coluna com chamada de um artigo em html
+function montaColuna(retorno, classeExtra, image, title, label, url, description){
+	retorno += '<div class="coluna ' + classeExtra + '">';
+	retorno += '<figure class="foto"><img src="assets/media/' + image + '" alt="' + title + '" title="' + title + '"><div class="imgbox"></div></figure>';
+	retorno += '<p class="label">' + label + '</p>';
+	retorno += '<h2><a href=" ' + url + '" title="' + title + '">' + title + '</a></h2>';
+	retorno += '<p class="description">' + description + '</p>';
+	retorno += '</div>';
+	return retorno;
+}
+
+//funcao para montar a chamada para os dois primeiros artigos da secao destaque em html
+function montaColunaDestaqueTop(retorno, image, title, label, url, description){
+	retorno += '<div class="coluna top" style="background: url(assets/media/' + image + '">';
+	retorno += '<div id="caixaArtigo">'; 
+	retorno += '<p class="label">' + label + '</p>';
+	retorno += '<h2><a href=" ' + url + '" title="' + title + '">' + title + '</a></h2>';
+	retorno += '<p class="description">' + description + '</p>';
+	retorno += '</div></div>';
+	return retorno;
+}
 
 //funcao para esconder e mostrar artigos ocultos de paginacao
 function toggle_visibility(id) {
@@ -90,8 +95,8 @@ function toggle_visibility(id) {
 }
 
 //funcao para inserir o texto do anuncio de acordo com o dispositivo
-function setAdvertisingInformation(){
-	if ($(window).width()>520) { $('#advertising').html("Advertising 728x90"); return; }
+function setAdvertisingInformation(breakSize){
+	if ($(window).width()>breakSize) { $('#advertising').html("Advertising 728x90"); return; }
 	$('#advertising').html('Advertising 320x100');
 }
 
