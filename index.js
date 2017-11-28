760d4d92f7e43413748f22a55de70bd030626ce2
eltonc.dev <eltonc.dev@gmail.com>
2017-11-27 23:11:09 -0200

    static main section

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 
 app.use('/', express.static('htdocs'));
+app.use('/node_modules', express.static('node_modules'))
 
 app.listen(3000, function() {
 	console.log('listen on http://localhost:3000');
